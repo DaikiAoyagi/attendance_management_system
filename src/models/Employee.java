591866dee28fb0c78478f97800c29ffbf8bd1a75
daiki.aoyagi <gaoyagi18@gmail.com>
2020-07-07 23:26:18 +0900

    Add Employee class

---
+++ src/models/Employee.java
@@ -0,0 +1,152 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Table(name = "employees")
+
+//checkRegisteredCode:指定された社員番号がすでにデータベースに存在しているか
+//checkLoginCodeAndPassword:従業員がログインするときに社員番号とパスワードが正しいかをチェック
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllEmployees",
+            query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
+            ),
+    @NamedQuery(
+            name = "getEmployeesCount",
+            query = "SELECT COUNT(e) FROM Employee AS e"
+            ),
+    @NamedQuery(
+            name = "checkRegisteredCode",
+            query = "SELECT COUNT(e) FROM Employee AS e WHERE e.employeeCode = :employeeCode"
+            ),
+    @NamedQuery(
+            name = "checkLoginCodeAndPassword",
+            query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.employeeCode = :employeeCode AND e.password = :pass"
+            )
+})
+
+/*
+id:リソース内での連番_数値型
+employeeCode:社員番号_文字列型
+employeeName:社員名_文字列型
+sectionCode:部署コード_文字列型
+password:システムへのログインパスワード_文字列型
+admin_flag:管理者権限があるかどうか_数値型（一般：0、管理者：1）
+created_at:登録日時_日時型
+updated_at:更新日時_日時型
+delete_flag:削除された従業員かどうか_数値型（現役：0、削除済み：1）
+ */
+
+@Entity
+public class Employee {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "employeeCode", nullable = false, unique = true)
+    private String employeeCode;
+
+    @Column(name = "employeeName", nullable = false)
+    private String employeeName;
+
+    @Column(name = "sectionCode", nullable = false)
+    private String sectionCode;
+
+    @Column(name = "password", length = 64, nullable = false)
+    private String password;
+
+    @Column(name = "admin_flag", nullable = false)
+    private Integer admin_flag;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
+    @Column(name = "delete_flag", nullable = false)
+    private Integer delete_flag;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public String getSectionCode() {
+        return sectionCode;
+    }
+
+    public void setSectionCode(String sectionCode) {
+        this.sectionCode = sectionCode;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAdmin_flag() {
+        return admin_flag;
+    }
+
+    public void setAdmin_flag(Integer admin_flag) {
+        this.admin_flag = admin_flag;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public Integer getDelete_flag() {
+        return delete_flag;
+    }
+
+    public void setDelete_flag(Integer delete_flag) {
+        this.delete_flag = delete_flag;
+    }
+}
---
+++ src/models/validators/EmployeeValidator.java
@@ -0,0 +1,83 @@
+package models.validators;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.persistence.EntityManager;
+
+import models.Employee;
+import utils.DBUtil;
+
+public class EmployeeValidator {
+    public static List<String> validate(Employee e, Boolean code_duplicate_check_flag, Boolean password_check_flag) {
+        List<String> errors = new ArrayList<String>();
+
+        String employeeCode_error = _validateCode(e.getEmployeeCode(), code_duplicate_check_flag);
+        if(!employeeCode_error.equals("")) {
+            errors.add(employeeCode_error);
+        }
+
+        String employeeName_error = _validateName(e.getEmployeeName());
+        if(!employeeName_error.equals("")) {
+            errors.add(employeeName_error);
+        }
+
+        String password_error = _validatePassword(e.getPassword(), password_check_flag);
+        if(!password_error.equals("")) {
+            errors.add(password_error);
+        }
+
+        return errors;
+    }
+
+
+    // 社員番号
+    private static String _validateCode(String employeeCode, Boolean code_duplicate_check_flag) {
+        // 必須入力チェック
+        if(employeeCode == null || employeeCode.equals("")) {
+            return "社員番号を入力してください。";
+        }else{
+            // 正規表現のパターンを作成
+            Pattern p = Pattern.compile("[0-9A-Z]{8}");
+            Matcher m = p.matcher(employeeCode);
+            if(m.find() == false){
+                return "社員番号は半角8桁の 大文字英字 と 数字の組み合わせで入力して下さい";
+            }
+        }
+
+        // すでに登録されている社員番号との重複チェック
+        if(code_duplicate_check_flag) {
+            EntityManager em = DBUtil.createEntityManager();
+            long employees_count = (long)em.createNamedQuery("checkRegisteredCode", Long.class)
+                                           .setParameter("employeeCode", employeeCode)
+                                             .getSingleResult();
+            em.close();
+            if(employees_count > 0) {
+                return "入力された社員番号の情報はすでに存在しています。";
+            }
+        }
+
+        return "";
+    }
+
+    // 社員名の必須入力チェック
+    private static String _validateName(String employeeName) {
+        if(employeeName == null || employeeName.equals("")) {
+            return "氏名を入力してください。";
+        }
+
+        return "";
+    }
+
+    // パスワードの必須入力チェック
+    private static String _validatePassword(String password, Boolean password_check_flag) {
+        // パスワードを変更する場合のみ実行
+        if(password_check_flag && (password == null || password.equals(""))) {
+            return "パスワードを入力してください。";
+        }
+        return "";
+    }
+}
